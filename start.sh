d426104b137d6076e4e7fa95bc8976eb47374b90
Brendan Mannix <u565@intrafile.com>
2015-10-02 11:32:51 -0700

    updates version of bmp

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 # Give server time to boot, then setup proxy on port 9091
 (sleep 3 && curl -sSX POST -d "port=9091&bindAddress=$(cat /etc/hosts | grep $(hostname) | cut -f1)" http://0.0.0.0:9090/proxy) &
 
-exec /browsermob-proxy-2.0-beta-9/bin/browsermob-proxy -port 9090
+exec /browsermob-proxy-2.1.0-beta-3/bin/browsermob-proxy -port 9090
